c81c3d13d07796593263061032f36e4d93bdd3d2
Henrique Dias <hvdias2015@gmail.com>
2022-09-13 08:31:54 -0300

    Added teste cases

--- source/main.cpp
+++ source/main.cpp
@@ -7,18 +7,25 @@ using namespace std;
 int main() {
     LinkedList TokenList;
     Node token;
-    FILE *file = openFile((char *) "../docs/gera1.txt");
-
+    FILE *file = openFile((char *) "../Lexico/teste_1.txt");
+    int c = 0;
     character = (char) fgetc(file);
     do {
         token = getToken(file);
-        if (!token.lexema.empty() && !token.simbolo.empty())
+        if (!token.lexema.empty() && !token.simbolo.empty()){
             TokenList.insertNode(token.lexema, token.simbolo);
-    } while (character != EOF);
+            c = 0;
+        }else{
+            c++;
+        }
+
+    } while (character != EOF && c != 10000);
 
     fclose(file);
 
     TokenList.printList();
-
+    if(c == 10000){
+        cout << "Lexema nao identificado" << endl;
+    }
     return 0;
 }
