f491843b3bc2af7d3e6e08987101bc288313c9af
Plendoga <vmancini77@gmail.com>
2022-09-15 18:09:32 -0300

    Started syntatic dev

---
+++ libs/lexicalAnalyser.h
@@ -0,0 +1,44 @@
+#ifndef COMPILER_LEXICAL_ANALYSER_H
+#define COMPILER_LEXICAL_ANALYSER_H
+
+#include "linkedList.h"
+
+using namespace std;
+
+extern char character;
+
+bool isSpace();
+
+bool isCommentary();
+
+bool isDigit();
+
+bool isLetter();
+
+bool isUnderscore();
+
+bool isArithmeticOperator();
+
+bool isRelationalOperator();
+
+bool isPunctuation();
+
+void jumpSpaces(FILE *file);
+
+void jumpComentary(FILE *file);
+
+Node handleDigit(FILE *file);
+
+Node handleIdAndSpecialWord(FILE *file);
+
+Node handleAttribution(FILE *file);
+
+Node handleArithmeticOperator(FILE *file);
+
+Node handleRelationalOperator(FILE *file);
+
+Node handlePunctuation(FILE *file);
+
+Node getToken(FILE *file);
+
+#endif //COMPILER_LEXICAL_ANALYSER_H
