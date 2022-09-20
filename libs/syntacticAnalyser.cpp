4c45299f5f6a6807049ca699b45bb09cf9b066fb
Plendoga <vmancini77@gmail.com>
2022-09-20 09:16:04 -0300

    Made syntaticAnalyser functions

--- libs/syntacticAnalyser.cpp
+++ libs/syntacticAnalyser.cpp
@@ -1,10 +1,8 @@
-#include <cstring>
 #include <string>
 #include "lexicalAnalyser.h"
 #include "syntacticAnalyser.h"
 
 using namespace std;
-
 
 Node analyseType(FILE* file, Node token) {
     if(token.simbolo != "sinteiro" && token.simbolo != "sbooleano") {
@@ -60,123 +58,48 @@ Node analyseEtVariables(FILE* file, Node token) {
     return token;
 }
 
-//----------------------------//
-
-Node analyseAtribChprocedimento(FILE* file, Node token){
-
-    token = getToken(file);
-    if (token.simbolo == "satribuição"){
-        analyseAtribuicao(file, token);
-    }else {
-        chamadaProcedimento(file, token);
-    }
-
-    return token;
-}
-
-Node analyseLeia(FILE* file, Node token){
-
-    token = getToken(file);
-    if (token.simbolo == "sabre_parenteses"){
-        token = getToken(file);
-        if (token.simbolo == "sidentificador"){
-            token = getToken(file);
-            if (token.simbolo == "sfecha_parenteses"){
-                token = getToken(file);
-            }else {
-                printf("Erro");
-            }
+Node analyseSubroutine(FILE* file, Node token) {
+    while(token.simbolo == "sprocedimento" || token.simbolo == "sfunção"){
+        if (token.simbolo == "sprocedimento"){
+            token = analyseProcedureDeclaration(file, token);
         }else {
-            printf("Erro");
+            token = analyseFunctionDeclaration(file, token);
         }
-    }else {
-        printf("Erro");
-    }
-
-    return token;
-}
-
-Node analyseEscreva(FILE* file, Node token){
-
-    token = getToken(file);
-    if (token.simbolo == "sabre_parenteses"){
-        token = getToken(file);
-        if (token.simbolo == "sidentificador"){
+        if (token.simbolo == "sponto-virgula"){
             token = getToken(file);
-            if (token.simbolo == "sfecha_parenteses"){
-                token = getToken(file);
-            }else {
-                printf("Erro");
-            }
         }else {
-            printf("Erro");
+            printf("ERRO");
         }
-    }else {
-        printf("Erro");
-    }
-
-    return token;
-}
-
-Node analyseEnquanto(FILE* file, Node token){
-
-    token = getToken(file);
-    analyseExpressao(file, token);
-    if (token.simbolo == "sfaça"){
-        token = getToken(file);
-        analyseSimpleCommands(file, token);
-    }else {
-        printf("Erro");
-    }
-
-    return token;
-}
-
-Node analyseSe(FILE* file, Node token){
-
-    token = getToken(file);
-    analyseExpressao(file, token);
-    if (token.simbolo == "sentão"){
-        token = getToken(file);
-        analyseSimpleCommands(file, token);
-        if (token.simbolo == "Ssenão"){
-            token = getToken(file);
-            analyseSimpleCommands(file, token);
-        }
-    }else {
-        printf("Erro");
     }
 
     return token;
 }
 
 Node analyseSimpleCommands(FILE* file, Node token) {
-
     if(token.simbolo == "sidentificador"){
-        analyseAtribChprocedimento(file, token);
+        token = analyseAttributionAndProcedureCall(file, token);
     }else if(token.simbolo == "sse"){
-        analyseSe(file, token);
+        token = analyseIf(file, token);
     }else if(token.simbolo == "senquanto"){
-        analyseEnquanto(file, token);
+        token = analyseWhile(file, token);
     }else if(token.simbolo == "sleia"){
-        analyseLeia(file, token);
+        token = analyseRead(file, token);
     }else if(token.simbolo == "sescreva"){
-        analyseEscreva(file, token);
+        token = analyseWrite(file, token);
     }else{
-        analyseCommands(file,token);
+        token = analyseCommands(file,token);
     }
 
     return token;
 }
 
 Node analyseCommands(FILE* file, Node token) {
-
     if (token.simbolo == "sinicio"){
         token = getToken(file);
-        analyseSimpleCommands(file,token);
+        token = analyseSimpleCommands(file,token);
         while(token.simbolo != "sfim"){
             if(token.simbolo == "spontovirgula"){
-                analyseSimpleCommands(file,token);
+                token = analyseSimpleCommands(file,token);
             }else{
                 printf("Erro");
             }
@@ -189,6 +112,223 @@ Node analyseCommands(FILE* file, Node token) {
     return token;
 }
 
-//----------------------------------------------//
+Node analyseBlock(FILE* file, Node token) {
+    token = getToken(file);
 
+    analyseEtVariables(file, token);
+    analyseSubroutine(file, token);
+    analyseCommands(file, token);
 
+    return token;
+}
+
+Node analyseProcedureDeclaration(FILE* file, Node token) {
+    token = getToken(file);
+
+    if(token.simbolo == "sidentificador") {
+        token = getToken(file);
+
+        if(token.simbolo == "sponto_virgula") {
+            token = analyseBlock(file, token);
+        } else {
+            printf("Erro");
+        }
+    } else {
+        printf("Erro");
+    }
+
+    return token;
+}
+
+Node analyseFunctionDeclaration(FILE* file, Node token) {
+    token = getToken(file);
+
+    if(token.simbolo == "sidentificador") {
+        token = getToken(file);
+
+        if(token.simbolo == "sdoispontos") {
+            token = getToken(file);
+
+            if(token.simbolo == "sinteiro" || token.simbolo == "sbooleano") {
+                token = getToken(file);
+
+                if(token.simbolo == "sponto_virgula") {
+                    token = analyseBlock(file, token);
+                } else {
+                    printf("Erro");
+                }
+            } else {
+                printf("Erro");
+            }
+        } else {
+            printf("Erro");
+        }
+    } else {
+        printf("Erro");
+    }
+
+    return token;
+}
+
+Node analyseFunctionCall(FILE* file, Node token){
+    return token;
+}
+
+Node analyseFactor(FILE* file, Node token){
+    if (token.simbolo == "sidentificador"){
+        token = analyseFunctionCall(file, token);
+    }else if(token.simbolo == "snumero"){
+        token = getToken(file);
+    }else if(token.simbolo == "snao"){
+        token = getToken(file);
+        token = analyseFactor(file, token);
+    }else if(token.simbolo == "sabre_parenteses"){
+        token = getToken(file);
+        token = analyseExpressions(file, token);
+        if (token.simbolo == "sfecha_parenteses"){
+            token = getToken(file);
+        }else {
+            printf("Erro");
+        }
+    } else if(token.lexema == "verdadeiro" || token.lexema == "falso") {
+        token = getToken(file);
+    } else {
+        printf("Erro");
+    }
+
+    return token;
+}
+
+Node analyseTerm(FILE* file, Node token) {
+    token = analyseFactor(file, token);
+
+    while(token.simbolo == "smult" || token.simbolo == "sdiv" || token.simbolo == "if") {
+        token = getToken(file);
+        token = analyseFactor(file, token);
+    }
+    return token;
+}
+
+Node analyseExpressions(FILE* file, Node token) {
+    token = analyseSimpleExpressions(file, token);
+    if (token.simbolo == "smaior" || token.simbolo == "smaiorig" ||
+    token.simbolo == "smenor" || token.simbolo == "smenorig" ||
+    token.simbolo == "sdif"){
+        token = getToken(file);
+        token = analyseSimpleExpressions(file, token);
+    }
+
+    return token;
+}
+
+Node analyseSimpleExpressions(FILE* file, Node token) {
+    if (token.simbolo == "smais" || token.simbolo == "smenos"){
+        token = getToken(file);
+        token = analyseTerm(file, token);
+        while (token.simbolo == "smais" || token.simbolo == "smenos" || token.simbolo == "sou"){
+            token = getToken(file);
+            token = analyseTerm(file, token);
+        }
+    }
+
+    return token;
+}
+
+Node analyseAttribution(FILE* file, Node token) {
+    token = getToken(file);
+
+    token = analyseExpressions(file, token);
+
+    return token;
+}
+
+Node analyseProcedureCall(FILE* file, Node token) {
+    return token;
+}
+
+Node analyseAttributionAndProcedureCall(FILE* file, Node token){
+    token = getToken(file);
+    if (token.simbolo == "satribuição"){
+        token = analyseAttribution(file, token);
+    }else {
+        token = analyseProcedureCall(file, token);
+    }
+
+    return token;
+}
+
+Node analyseRead(FILE* file, Node token){
+
+    token = getToken(file);
+    if (token.simbolo == "sabre_parenteses"){
+        token = getToken(file);
+        if (token.simbolo == "sidentificador"){
+            token = getToken(file);
+            if (token.simbolo == "sfecha_parenteses"){
+                token = getToken(file);
+            }else {
+                printf("Erro");
+            }
+        }else {
+            printf("Erro");
+        }
+    }else {
+        printf("Erro");
+    }
+
+    return token;
+}
+
+Node analyseWrite(FILE* file, Node token) {
+    token = getToken(file);
+
+    if (token.simbolo == "sabre_parenteses"){
+        token = getToken(file);
+        if (token.simbolo == "sidentificador"){
+            token = getToken(file);
+            if (token.simbolo == "sfecha_parenteses"){
+                token = getToken(file);
+            }else {
+                printf("Erro");
+            }
+        }else {
+            printf("Erro");
+        }
+    }else {
+        printf("Erro");
+    }
+
+    return token;
+}
+
+Node analyseWhile(FILE* file, Node token){
+
+    token = getToken(file);
+    token = analyseExpressions(file, token);
+    if (token.simbolo == "sfaça"){
+        token = getToken(file);
+        token = analyseSimpleCommands(file, token);
+    }else {
+        printf("Erro");
+    }
+
+    return token;
+}
+
+Node analyseIf(FILE* file, Node token){
+
+    token = getToken(file);
+    token = analyseExpressions(file, token);
+    if (token.simbolo == "sentão"){
+        token = getToken(file);
+        token = analyseSimpleCommands(file, token);
+        if (token.simbolo == "Ssenão"){
+            token = getToken(file);
+            token = analyseSimpleCommands(file, token);
+        }
+    }else {
+        printf("Erro");
+    }
+
+    return token;
+}
--- libs/syntacticAnalyser.h
+++ libs/syntacticAnalyser.h
@@ -1,4 +1,41 @@
 #ifndef COMPILER_SYNTACTICANALYSER_H
 #define COMPILER_SYNTACTICANALYSER_H
 
+Node analyseType(FILE* file, Node token);
+
+Node analyseVariables(FILE* file, Node token);
+
+Node analyseEtVariables(FILE* file, Node token);
+
+Node analyseProcedureDeclaration(FILE* file, Node token);
+
+Node analyseFunctionDeclaration(FILE* file, Node token);
+
+Node analyseSubroutine(FILE* file, Node token);
+
+Node analyseCommands(FILE* file, Node token);
+
+Node analyseFunctionCall(FILE* file, Node token);
+
+Node analyseFactor(FILE* file, Node token);
+
+Node analyseTerm(FILE* file, Node token);
+
+Node analyseExpressions(FILE* file, Node token);
+
+Node analyseSimpleExpressions(FILE* file, Node token);
+
+Node analyseAttribution(FILE* file, Node token);
+
+Node analyseProcedureCall(FILE* file, Node token);
+
+Node analyseAttributionAndProcedureCall(FILE* file, Node token);
+
+Node analyseRead(FILE* file, Node token);
+
+Node analyseWrite(FILE* file, Node token);
+
+Node analyseWhile(FILE* file, Node token);
+
+Node analyseIf(FILE* file, Node token);
 #endif //COMPILER_SYNTACTICANALYSER_H
